8e5b166d93a0c48eda542d132b17efc690d613be
ARAVINDH-1505 <srinivasaaravindh15@gmail.com>
2026-07-03 12:34:59 +0530

    Implemented embedder function

---
+++ embedder.py
@@ -0,0 +1,20 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def create_embeddings(chunks: list[str]) -> list[list[float]]:
+    embeddings = model.encode(chunks)
+    return embeddings.tolist()
+
+
+if __name__ == "__main__":
+    from reader import find_files
+    from splitter import split_file_into_chunks
+
+    files = find_files("D:\VELAI THEDUM PADALAM\MCP", file_types=[".py", ".md", ".json"])
+
+    for file in files:
+        chunks = split_file_into_chunks(file)
+        embeddings = create_embeddings(chunks)
+        print(f"{file} -> {len(chunks)} chunks -> {len(embeddings)} embeddings")
+        print(f"Each embedding has {len(embeddings[0])} numbers")
